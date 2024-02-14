d525e49507d59e4c2b3f11f03bc3e168af699f17
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-02-14 23:08:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,55 @@
-import streamlit as st
-from datetime import datetime
+#Not bu kod benim değil. Bi videoda gördüm. Yapay zeka çalışması. Örnek kalsın diye burada tutuyorum.
+#Umarım bi ara bu kütüphaneyi öğrendiğim zamanlar gelecek.
+
 import yfinance as yf 
-import matplotlib.pyplot as plt
+import datetime
+from datetime import date 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error
 
-favorites = ['TUPRS', 'MIATK', 'ALFAS']
+def download_data(op,start_date,end_date):
+    df = yf.download(op,start=start_date,end=end_date)
+    return df
 
-manualSelect = st.sidebar.selectbox('Keremin Sevdiği Hisseler', favorites + ['Kendim seçecem ihtiyar'])
+def model_engine(model,num):
+    df  = data[['Close']]
+    df['preds'] = df.Close.shift(-num) #bir tahmin sütunu oluşturuyor. Bir sonraki günü predictions sütununa aktarıyor. shift o işe yarıyor.
 
-if manualSelect == 'Kendim seçecem ihtiyar':
-    symbol = st.sidebar.text_input("Hisse senedi sembolü", value='TUPRS')
-else:
-    symbol = manualSelect
+    # Eksen oluşturma
+    x = df.drop(['preds'],axis=1).values
+    x = scaler.fit_transform(x)
+    x = x[:-num] #num gün öncesi kadar gibi bi anlamı var
+    x_forecast = x[-num:] 
+    y = df.preds.values
+    y = y[:-num]
 
-st.title(symbol+' Hissesinin grafiği')
+    #Modeli eğitme ve tahmin ettirme
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=7)
+    model.fit(x_train,y_train)
+    preds = model.predict(x_test)
+    print(f'Tutarlılık oranı şu şekilde tahmin edildi: {r2_score(y_test,preds)}')
 
-startDate = st.sidebar.date_input('Başlangıç tarihi',value=datetime(2020,3,26))
-endDate = st.sidebar.date_input('Başlangıç tarihi',value=datetime.now())
+    #Tahmini söyletme
+    forecasted_pred = model.predict(x_forecast)
+    day = 1
+    for i in forecasted_pred:
+        print(f'{day}. gün için tahmini kapanış değeri: {i}')
+        day += 1
 
-getStocks = yf.download(symbol+'.IS',start=startDate,end=endDate)
-st.subheader("Trend Grafiği")
-st.line_chart(getStocks['Close'])
 
-st.subheader('Hisse tablosu')
-st.write(getStocks)
+stock = 'TUPRS.IS'
+today = datetime.date.today() #Bugünü döndürüyor (2024-02-11)
+duration = 3000
+before = today - datetime.timedelta(days=duration) #yukarıda duration 3000 belirledik. bu kod today'den 3000 gün geri gidiyor.
+start_date = before
+end_date = today
+
+data = download_data(stock,start_date,end_date)
+
+num=3 #Kaç günlük tahmin yapacaksa
+scaler = StandardScaler()
+engine = LinearRegression()
+
+model_engine(engine,num)
